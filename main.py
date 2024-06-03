c646507ca250cac8b94e99f4e4ffb91934dd781c
margotriou <margot.riou56@gmail.com>
2024-06-03 17:05:32 +0200

    Faut tout mettre en UTF-8 et ça marche pour le read

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def question1_2():
     n_objets = [1, 2, 10, 23]
     print("Question 1 :")
     for i in n_objets:
-        print("Nombre de possibilités pour", i, "objets:", 2**i)
-    print("\nQuestion2 :\nNombre max de possibilités :", 2**max(n_objets))
+        print("Nombre de possibilitÃ©s pour", i, "objets:", 2**i)
+    print("\nQuestion2 :\nNombre max de possibilitÃ©s :", 2**max(n_objets))
 
 
 # Question 3 :
-# Utilité maximale pour un poids inférieur ou égal à 0.6 : 7.6 avec 6 objets pour un poids total de 0.6
+# UtilitÃ© maximale pour un poids infÃ©rieur ou Ã©gal Ã  0.6 : 7.6 avec 6 objets pour un poids total de 0.6
 
 
 if __name__ == '__main__':

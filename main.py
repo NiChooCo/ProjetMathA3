5cc0e9177249ac0c61970f04a440d709e55227f1
NiChooCo <nicolas.taillebois@isen-ouest.yncrea.fr>
2024-06-03 15:00:30 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def question1_2():
+    n_objets = [1, 2, 10, 23]
+    print("Question 1 :")
+    for i in n_objets:
+        print("Nombre de possibilités pour", i, "objets:", 2**i)
+    print("\nQuestion2 :\nNombre max de possibilités :", 2**max(n_objets))
+
+
+if __name__ == '__main__':
+    question1_2()

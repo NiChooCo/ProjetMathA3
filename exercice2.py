04ac8d448915aa03b611992bfd0d7229c5a0d613
margotriou <margot.riou56@gmail.com>
2024-06-04 15:07:21 +0200

    exo2

---
+++ exercice2.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+data = pd.read_csv("Donnees_marchandises.csv", header=1, delimiter=";")
+def online1():
+    """
+    Rangement en ne prenant en compte que la longeur des marchandises et on ne peut pas les trier au départ.
+    :return:
+    """
+    pass
+
+def online2():
+    """
+    Rangement en ne prenant en compte que la longeur et la largeur des marchandises et on ne peut pas les trier au départ.
+    :return:
+    """
+    pass
+
+def online3():
+    """
+    Rangement en ne prenant en compte toutes les dimmensions (longeur, largeur, hauteur) des marchandises et on ne peut
+    pas les trier au départ.
+    :return:
+    """
+    pass
+
+def offline1():
+    """
+    Rangement en ne prenant en compte que la longeur des marchandises et on peut les trier au départ
+    :return:
+    """
+    pass
+
+def offline2():
+    """
+    Rangement en ne prenant en compte que la longeur des marchandises et on peut les trier au départ
+    :return:
+    """
+    pass
+
+def offline3():
+    """
+    Rangement en ne prenant en compte toutes les dimmensions (longeur, largeur, hauteur) des marchandises et on peut les
+    trier au départ
+    :return:
+    """
+    pass
